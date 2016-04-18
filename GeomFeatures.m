f01a8b8782a89e9ca49181c2bbe7ae297673ae58
Alex Kreimer <alex.kreimer@gmail.com>
2016-04-18 11:52:08 +0300

    Initial commit

---
+++ GeomFeatures.m
@@ -0,0 +1,50 @@
+classdef GeomFeatures
+    % implementation of geometric features used in this work
+    % www.iai.uni-bonn.de/~gall/download/jgall_poseAR_bmvc11.pdf
+    properties
+        traj
+    end
+    methods
+        function obj = GeomFeatures(traj)
+            if nargin>0
+                obj.traj = traj;
+            end
+        end
+        
+        function val = F_jd(obj, j1, j2, t1, t2)
+            % joint distance feature
+            p1 = obj.traj(:,j1,t1);
+            p2 = obj.traj(:,j2,t2);
+            
+            val = norm(p1-p2);
+        end
+        
+        function val = F_pl(obj, j1, j2, j3, j4, t1, t2)
+            % plane feature
+
+            p1 = obj.traj(:,j1,t1);
+            p2 = obj.traj(:,j2,t2);
+            p3 = obj.traj(:,j3,t2);
+            p4 = obj.traj(:,j4,t2);
+            
+            n = cross(p2-p3, p2-p4);
+            n = n/norm(n);
+
+            d = -n'*p2;
+            val = [n;d]'*[p1;1];
+        end
+        
+        function val = F_np(obj, j1, j2, j3, j4, t1, t2)
+            % normal plane feature
+            p1 = obj.traj(:,j1,t1);
+            p2 = obj.traj(:,j2,t2);
+            p3 = obj.traj(:,j3,t2);
+            p4 = obj.traj(:,j4,t2);
+
+            n = (p2-p3)/norm(p2-p3);
+            d = -n'*p4;
+            
+            val = [n;d]'*[p1;1];
+        end
+    end
+end
